9d87a3702fbb3c1a9fce6a742207972afd738031
Allen Wenzl <allenliwenzl@gmail.com>
2023-02-06 11:29:06 -0500

    update imports

--- project1b.py
+++ project1b.py
@@ -1,13 +1,8 @@
-import time
-import cv2
-from tflite_support.task import core
-from tflite_support.task import processor
-from tflite_support.task import vision
 from navigation import make_scan_list, make_safety_zone, a_star, make_route, generate_instructions, generate_movements
-import utils
 import numpy as np
 from car import us_sweep, startup, stop_car
 from viz import start_camera, get_detect, parse_detects
+import car
 
 startup()
 cap = start_camera()
@@ -23,7 +18,6 @@ mapping[goal] = 9
 angles = np.deg2rad(np.arange(-60,70,10)) #in radians
 xvals = (np.round((np.sin(angles) * sweep)/5, 0)+14).astype(int)
 yvals = ((np.cos(angles) * sweep)//5).astype(int)
-
 
 scan_list = make_scan_list(list(zip(yvals, xvals))) 
 for pair in scan_list:
